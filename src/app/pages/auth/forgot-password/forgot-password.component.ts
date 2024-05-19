95df6d9bb90563b4f39b8b23d919a88e8194eefb
JuanSCaicedo <sebanchit@gmail.com>
2024-05-19 17:55:07 -0500

    recuperar password parte 2

--- src/app/pages/auth/forgot-password/forgot-password.component.ts
+++ src/app/pages/auth/forgot-password/forgot-password.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CodeForgotPasswordComponent } from '../code-forgot-password/code-forgot-password.component';
+import { NewPasswordComponent } from '../new-password/new-password.component';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [CommonModule, CodeForgotPasswordComponent],
+  imports: [CommonModule, CodeForgotPasswordComponent, NewPasswordComponent],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.css'
 })
 export class ForgotPasswordComponent {
-  
+  isLoadingMail: any = null;
+  isLoadingCode: any = null;
 }
