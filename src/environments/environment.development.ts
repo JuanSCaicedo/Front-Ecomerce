62b6d0075de2c0703ee986fb45e2198e30b74436
JuanSCaicedo <sebanchit@gmail.com>
2024-06-27 21:37:27 -0500

    modificando ruta del proyecto a ip local

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    URL_BACKEND: 'http://127.0.0.1:8000',
-    URL_SERVICIOS: 'http://127.0.0.1:8000/api',
-    URL_FRONTEND: 'http://localhost:5000',
+    URL_BACKEND: 'http://192.168.10.29:8000',
+    URL_SERVICIOS: 'http://192.168.10.29:8000/api',
+    URL_FRONTEND: 'http://192.168.10.29:5000',
 };
