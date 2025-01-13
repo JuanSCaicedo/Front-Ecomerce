e99cb9de16d1c13c3004475d3d6fd14e8c262269
JuanScaicedo <sebanchit@gmail.com>
2025-01-13 08:48:03 -0500

    agregando funcion ir hacia arriba para la /

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -10,4 +10,16 @@ import { RouterModule } from '@angular/router';
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
+
+  constructor(private router: Router) { }
+
+  navigateToHome() {
+    if (this.router.url === '/') {
+      // Si ya estás en el home, desplázate hacia arriba
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+      // Si no estás en el home, redirige al home
+      this.router.navigate(['/']);
+    }
+  }
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MenuCategoriesComponent } from './menu-categories/menu-categories.component';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,5 +11,15 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+    constructor(private router: Router) { }
+  
+    navigateToHome() {
+      if (this.router.url === '/') {
+        // Si ya estás en el home, desplázate hacia arriba
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      } else {
+        // Si no estás en el home, redirige al home
+        this.router.navigate(['/']);
+      }
+    }
 }
