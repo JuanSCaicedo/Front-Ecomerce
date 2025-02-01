7b0cea9262a303788d8944ce43eed64af7dcfd93
JuanScaicedo <sebanchit@gmail.com>
2025-01-31 20:42:42 -0500

    areglando bug de cantidad de producto desde la landing

--- src/app/pages/home/modal-producto/modal-producto.component.ts
+++ src/app/pages/home/modal-producto/modal-producto.component.ts
@@ -217,10 +217,10 @@ export class ModalProductoComponent {
       code_cupon: null,
       code_discount: code_discount_v,
       product_variation_id: product_variation_id,
-      quantity: $("#tp-cart-input-val").val(),
+      quantity: $("#tp-cart-input-val-modal").val(),
       price_unit: this.currency == 'COP' ? this.product_selected.price_cop : this.product_selected.price_usd,
       subtotal: subtotal_v,
-      total: subtotal_v * $("#tp-cart-input-val").val(),
+      total: subtotal_v * $("#tp-cart-input-val-modal").val(),
       currency: this.currency,
     }
 
