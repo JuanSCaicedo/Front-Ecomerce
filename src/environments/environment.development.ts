225c62ad0e225baf49780b73123b198abb201e68
JuanScaicedo <sebanchit@gmail.com>
2025-02-06 10:52:05 -0500

    cambiando variable de entorno para envio a produccion

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    URL_BACKEND: 'https://api-ecommerce.juandevops.com',
-    URL_SERVICIOS: 'https://api-ecommerce.juandevops.com/api',
+    URL_BACKEND: 'http://10.8.0.2:8000',
+    URL_SERVICIOS: 'http://10.8.0.2:8000/api',
     URL_FRONTEND: 'http://10.8.0.2:5000',
 };
