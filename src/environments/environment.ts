826eb842e44b78b6810867dadff8847ea477c37d
JuanScaicedo <sebanchit@gmail.com>
2025-02-14 21:21:59 -0500

    modificando variables de entorno de produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    URL_BACKEND: 'http://127.0.0.1:8000',
-    URL_SERVICIOS: 'http://127.0.0.1:8000/api',
-    URL_FRONTEND: 'http://localhost:5000',
+    URL_BACKEND: 'http://api-ecommerce.juandevops.com',
+    URL_SERVICIOS: 'http://api-ecommerce.juandevops.com/api',
+    URL_FRONTEND: 'https://front-ecomerce.pages.dev',
 };
