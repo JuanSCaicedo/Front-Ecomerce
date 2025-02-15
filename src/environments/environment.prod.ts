c6724dcb630871edd716160f0efe5187475f9377
JuanScaicedo <sebanchit@gmail.com>
2025-02-14 22:01:18 -0500

    actulizando variables de entorno en prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    URL_BACKEND: 'http://api-ecommerce.juandevops.com',
-    URL_SERVICIOS: 'http://api-ecommerce.juandevops.com/api',
+    URL_BACKEND: 'https://api-ecommerce.juandevops.com',
+    URL_SERVICIOS: 'https://api-ecommerce.juandevops.com/api',
     URL_FRONTEND: 'https://front-ecomerce.pages.dev',
 };
