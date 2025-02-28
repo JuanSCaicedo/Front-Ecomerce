20f786a8c844d18e105561b8b581f6c5112e0c45
JuanScaicedo <sebanchit@gmail.com>
2025-02-28 17:55:54 -0500

    Actualizar componente de órdenes del perfil del cliente: modificar la tabla para mostrar detalles de transacciones, métodos de pago y agregar gestión de detalles de venta.

--- src/app/pages/view-auth/profile-client/orders-profile-client/orders-profile-client.component.ts
+++ src/app/pages/view-auth/profile-client/orders-profile-client/orders-profile-client.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { ProfileClientService } from '../service/profile-client.service';
+import { AuthService } from '../../../auth/service/auth.service';
+import { CartService } from '../../../home/service/cart.service';
+import { HomeService } from '../../../home/service/home.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-orders-profile-client',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './orders-profile-client.component.html',
   styleUrl: './orders-profile-client.component.css'
 })
 export class OrdersProfileClientComponent {
+  sales: any = [];
 
+  constructor(
+    public profileCliente: ProfileClientService,
+    public authService: AuthService,
+    public cartService: CartService,
+    public homeService: HomeService,
+    public toastr: ToastrService,
+  ) {
+    this.showOrders();
+  }
+
+  showOrders() {
+    this.profileCliente.showOrders().subscribe((resp: any) => {
+      console.log(resp);
+      this.sales = resp.sales.data;
+    }, (error) => {
+      if (error.status == 401) {
+        this.authService.sessionExpired();
+        this.cartService.clearCart();
+      } else if (error.status == 503) {
+        this.homeService.homeView('SYSTEM_MAINTENANCE_ACTIVE').subscribe();
+      } else if (error.status == 429) {
+        this.toastr.error("Demasiadas solicitudes. Por favor, espere unos segundos.", "Error de solicitud");
+        return;
+      } else {
+        console.log(error);
+        this.toastr.error('API Response - Comuníquese con el desarrollador', error.error.message || error.message);
+      }
+    });
+  }
+
+  detailShow(sale: any) {
+    sale.sale_detail_show = !sale.sale_detail_show;
+  }
 }
