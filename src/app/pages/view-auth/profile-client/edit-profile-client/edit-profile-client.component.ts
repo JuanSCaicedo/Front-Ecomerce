9a7af0064de4b33e519a2491a3117398009c80c8
JuanScaicedo <sebanchit@gmail.com>
2025-02-27 20:22:51 -0500

    Agregar funcionalidad para editar el perfil del cliente y mejorar la gestión de datos del formulario

--- src/app/pages/view-auth/profile-client/edit-profile-client/edit-profile-client.component.ts
+++ src/app/pages/view-auth/profile-client/edit-profile-client/edit-profile-client.component.ts
@@ -1,12 +1,76 @@
 import { Component } from '@angular/core';
+import { ProfileClientService } from '../service/profile-client.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../auth/service/auth.service';
+import { CartService } from '../../../home/service/cart.service';
+import { HomeService } from '../../../home/service/home.service';
 
 @Component({
   selector: 'app-edit-profile-client',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-profile-client.component.html',
   styleUrl: './edit-profile-client.component.css'
 })
 export class EditProfileClientComponent {
 
+  name: string = '';
+  surname: string = '';
+  email: string = '';
+  phone: string = '';
+  bio: string = '';
+  fb: string = '';
+  tw: string = '';
+  sexo: string = '';
+  description: string = '';
+  address_city: string = '';
+  file_imagen: any;
+  imagen_previsualiza: any;
+
+  constructor(
+    public profileClient: ProfileClientService,
+    public toastr: ToastrService,
+    public authService: AuthService,
+    public cartService: CartService,
+    public homeService: HomeService,
+  ) {
+    this.showUser();
+  }
+
+  updateProfile() {
+
+  }
+
+  showUser() {
+    this.profileClient.showUsers().subscribe((resp: any) => {
+      console.log(resp);
+      this.name = resp.name
+      this.surname = resp.surname
+      this.email = resp.email
+      this.phone = resp.phone
+      this.bio = resp.bio
+      this.fb = resp.fb
+      this.tw = resp.tw
+      this.sexo = resp.sexo
+      this.address_city = resp.address_city
+      this.imagen_previsualiza = resp.avatar;
+    }, (error) => {
+      // Manejo de errores según el código de estado
+      if (error.status == 401) {
+        this.authService.sessionExpired();
+        this.cartService.clearCart();
+      } else if (error.status == 503) {
+        this.homeService.homeView('SYSTEM_MAINTENANCE_ACTIVE').subscribe();
+      } else if (error.status == 429) {
+        this.toastr.error("Demasiadas solicitudes. Por favor, espere unos segundos.", "Error de solicitud");
+        return;
+      }
+      else {
+        console.log(error);
+        this.toastr.error('API Response - Comuniquese con el desarrollador', error.error.message || error.message);
+      }
+    });
+  }
 }
---
+++ src/app/pages/view-auth/profile-client/service/profile-client.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from '../../../auth/service/auth.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { URL_SERVICIOS } from '../../../../config/config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileClientService {
+
+  token: any;
+
+  constructor(
+    public authService: AuthService,
+    public http: HttpClient,
+  ) {
+    this.authService.tokenSubject.subscribe((token) => {
+      this.token = token; // Actualiza el token del servicio
+    });
+  }
+
+  getInforProfileClient() {
+    let headers = new HttpHeaders({
+      "Authorization": `Bearer ${this.token}` // Usa el token actualizado
+    });
+    let URL = URL_SERVICIOS + "/ecommerce/profile_client/";
+    return this.http.get(URL, { headers: headers });
+  }
+
+  updateProfile(data: any) {
+    let headers = new HttpHeaders({
+      "Authorization": `Bearer ${this.token}` // Usa el token actualizado
+    });
+    let URL = URL_SERVICIOS + "/ecommerce/profile_client/";
+    return this.http.put(URL, data, { headers: headers });
+  }
+
+  showUsers() {
+    let headers = new HttpHeaders({
+      "Authorization": `Bearer ${this.token}` // Usa el token actualizado
+    });
+    let URL = URL_SERVICIOS + "/ecommerce/profile_client/me";
+    return this.http.get(URL, { headers: headers });
+  }
+}
